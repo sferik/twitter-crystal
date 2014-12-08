fe13a07f875c12d01635e25d9d48941b1f7084ed
Erik Michaels-Ober <sferik@gmail.com>
2014-12-09 00:10:39 +0100

    Use OAuth::Consumer to sign requests

--- src/twitter/rest/client.cr
+++ src/twitter/rest/client.cr
@@ -1,8 +1,8 @@
-require "json"
-require "http/client"
-require "oauth"
 require "./timelines"
 require "./users"
+require "http/client"
+require "json"
+require "oauth"
 require "uri"
 
 module Twitter
@@ -11,31 +11,28 @@ module Twitter
       include Twitter::REST::Users
       include Twitter::REST::Timelines
       Host = "api.twitter.com"
-      Port = 443
-      SSL  = true
       property :access_token, :access_token_secret, :consumer_key, :consumer_secret, :user_agent
 
       def initialize(@consumer_key : String, @consumer_secret : String, @access_token : String, @access_token_secret : String, @user_agent = nil : Nil | String)
         @user_agent ||= "CrystalTwitterClient/#{Twitter::Version.to_s}"
-        @connection = HTTP::Client.new(Host, Port, SSL)
+        consumer = OAuth::Consumer.new(Host, consumer_key, consumer_secret)
+        access_token = OAuth::AccessToken.new(access_token, access_token_secret)
+        @http_client = HTTP::Client.new(Host, ssl: true)
+        consumer.authenticate(@http_client, access_token)
       end
 
       def get(path : String, params = {} of String => String)
         path += "?#{to_query_string(params)}" unless params.empty?
-        exec("GET", path)
+        response = @http_client.get(path)
+        handle_response(response)
       end
 
-      def post(path : String, params = {} of String => String)
-        body = to_query_string(params)
-        exec("POST", path, body)
+      def post(path : String, form = {} of String => String)
+        response = @http_client.post_form(path, form)
+        handle_response(response)
       end
 
-      private def exec(method : String, path : String, body = nil : String)
-        request = HTTP::Request.new(method, path, HTTP::Headers.new, body)
-        request.headers["Content-Type"] = "application/x-www-form-urlencoded"
-        request.headers["Host"] = Host
-        request.headers["Authorization"] = authorization_header(request)
-        response = @connection.exec(request)
+      private def handle_response(response : HTTP::Response)
         case response.status_code
         when 200..299
           response.body
@@ -53,13 +50,6 @@ module Twitter
         else
           ""
         end
-      end
-
-      private def authorization_header(request)
-        signature = OAuth::Signature.new(consumer_key, consumer_secret, oauth_token: access_token, token_shared_secret: access_token_secret)
-        nonce = (0...42).map { (('0'..'9').to_a + ('A'..'Z').to_a + ('a'..'z').to_a)[rand(62)] }.join
-        timestamp = Time.now.to_i.to_s
-        signature.authorization_header(request, SSL, timestamp, nonce)
       end
 
       private def to_query_string(hash : Hash)
