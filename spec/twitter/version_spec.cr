212be7ee80d4a9ea246ebd251634ec1a0ed8500e
Erik Michaels-Ober <sferik@gmail.com>
2015-08-29 16:41:34 +0200

    Add specs for Twitter::User

--- spec/twitter/version_spec.cr
+++ spec/twitter/version_spec.cr
@@ -6,12 +6,6 @@ describe Twitter::Version do
       Twitter::Version.to_s.split('.').count.should eq(3)
     end
 
-    it "each segment of the the string is numeric" do
-      Twitter::Version.to_s.split('.').each do |number|
-        (/^\d+$/ =~ number).should eq(0)
-      end
-    end
-
     it "each segment of the string is in the right position" do
       x, y, z = Twitter::Version.to_s.split('.')
       x.to_i.should eq(Twitter::Version::Major)
