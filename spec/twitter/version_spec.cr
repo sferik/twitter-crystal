41cc42020aca0cfc8a5f328f85dccb3542d39c3a
Matthew Rothenberg <mrothenberg@gmail.com>
2015-11-06 16:36:06 -0500

    Enumerable.count -> Enumerable.size
    it looks like the semantics must have changed here, and `count` is now
    for looking for a specific condition, rather than just returning number
    of elements in Enumerable.

--- spec/twitter/version_spec.cr
+++ spec/twitter/version_spec.cr
@@ -3,7 +3,7 @@ require "../helper"
 describe Twitter::Version do
   describe "#to_s" do
     it "is in the form X.Y.Z" do
-      Twitter::Version.to_s.split('.').count.should eq(3)
+      Twitter::Version.to_s.split('.').size.should eq(3)
     end
 
     it "each segment of the string is in the right position" do
