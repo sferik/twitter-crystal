40894c727a4cc07c4c83e7060539f54d6d4a8398
kenta-s <knt01222@gmail.com>
2017-08-19 15:41:47 +0900

    Make users_spec work

--- spec/helper.cr
+++ spec/helper.cr
@@ -1,2 +1,3 @@
 require "spec"
 require "../src/twitter-crystal"
+require "./mock/client"
--- spec/twitter/rest/users_spec.cr
+++ spec/twitter/rest/users_spec.cr
@@ -1,80 +1,63 @@
-pending do
-  describe Twitter::REST::Users do
-    describe "#user" do
-      client = Twitter::REST::Client.new("CK", "CS", "AT", "AS", "UA")
+describe Twitter::REST::Users do
+  client = Mock::Client.new("CK", "CS", "AT", "AS", "UA")
 
-      context "without any args" do
-        it "returns user: 7505382" do
-          client.user.id.should eq 7505382
-        end
-      end
+  describe "#user" do
 
-      context "with 776284343173906432" do
-        it "returns user: 776284343173906432" do
-          client.user(776284343173906432).id.should eq 776284343173906432
-        end
-      end
-
-      context "with 'sferik'" do
-        it "returns 7505382" do
-          client.user("sferik").id.should eq 7505382
-        end
+    context "without any args" do
+      it "returns user: 7505382" do
+        client.user.id.should eq 7505382
       end
     end
 
-    describe "#users" do
-      client = Twitter::REST::Client.new("CK", "CS", "AT", "AS", "UA")
-
-      context "with 'kenta_s_dev'" do
-        users = client.users("kenta_s_dev")
-        it "returns user: 776284343173906432" do
-          users.size.should eq 1
-          users.first.id.should eq 776284343173906432
-        end
-      end
-
-      context "with 776284343173906432" do
-        users = client.users(776284343173906432)
-        it "returns user: 776284343173906432" do
-          users.size.should eq 1
-          users.first.id.should eq 776284343173906432
-        end
-      end
-
-      context "with 'sferik'" do
-        users = client.users("sferik")
-        it "returns user: 7505382" do
-          users.size.should eq 1
-          users.first.id.should eq 7505382
-        end
-      end
-
-      context "with 7505382" do
-        users = client.users(7505382)
-        it "returns user: 7505382" do
-          users.size.should eq 1
-          users.first.id.should eq 7505382
-        end
+    context "with  7505382" do
+      it "returns user:  7505382" do
+        client.user.id.should eq 7505382
       end
     end
 
-    describe "#user_search" do
-      client = Twitter::REST::Client.new("CK", "CS", "AT", "AS", "UA")
-
-      context "with 'power zero-sum'" do
-        users = client.user_search("power zero-sum")
-        it "returns user: 7505382" do
-          users.size.should eq 1
-          users.first.id.should eq 7505382
-        end
+    context "with 'sferik'" do
+      it "returns 7505382" do
+        client.user("sferik").id.should eq 7505382
       end
+    end
+  end
 
-      context "with 'Hello'" do
-        users = client.user_search("Hello")
-        it "returns user: 776284343173906432" do
-          users.size.should eq 1
-          users.first.id.should eq 776284343173906432
-        end
+  describe "#users" do
+    context "with String" do
+      users = client.users("hello")
+      it "returns Array(Twitter::User)" do
+        users.should be_a Array(Twitter::User)
+        users[0].screen_name.should eq "sferik"
+        users[1].screen_name.should eq "kenta_s_dev"
+      end
+    end
+
+    context "with Int64" do
+      users = client.users(123451234512345123)
+      it "returns Array(Twitter::User)" do
+        users.should be_a Array(Twitter::User)
+        users[0].screen_name.should eq "sferik"
+        users[1].screen_name.should eq "kenta_s_dev"
+      end
+    end
+
+    context "with Int32" do
+      users = client.users(12345)
+      it "returns Array(Twitter::User)" do
+        users.should be_a Array(Twitter::User)
+        users[0].screen_name.should eq "sferik"
+        users[1].screen_name.should eq "kenta_s_dev"
+      end
+    end
+  end
+
+  describe "#user_search" do
+    context "with String" do
+      users = client.user_search("Hello World")
+      it "returns Array(Twitter::User)" do
+        users.should be_a Array(Twitter::User)
+        users[0].screen_name.should eq "sferik"
+        users[1].screen_name.should eq "kenta_s_dev"
       end
     end
   end
