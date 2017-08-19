c6bd928e82ee4645e0af6c0a907a161a601550bc
kenta-s <knt01222@gmail.com>
2017-08-19 09:34:30 +0900

    Implement `retweet` and `unretweet`

--- src/twitter/rest/tweets.cr
+++ src/twitter/rest/tweets.cr
@@ -1,18 +1,36 @@
 module Twitter
   module REST
-    module Tweets
+    module Tweets # a.k.a Statuses
       def update(status : String, options = {} of String => String) : Twitter::Tweet
         response = post("/1.1/statuses/update.json", options.merge({ "status" => status }))
         Twitter::Tweet.from_json(response)
       end
 
       def destroy_status(tweet_id : Int32 | Int64, options = {} of String => String) : Twitter::Tweet
-        response = post("/1.1/statuses/destroy/#{tweet_id}.json", options)
+        response = post("/1.1/statuses/destroy.json", options.merge({ "id" => tweet_id.to_s }))
         Twitter::Tweet.from_json(response)
       end
 
       def destroy_status(tweet : Twitter::Tweet, options = {} of String => String) : Twitter::Tweet
         destroy_status(tweet.id, options)
+      end
+
+      def retweet(tweet_id : Int32 | Int64, options = {} of String => String) : Twitter::Tweet
+        response = post("/1.1/statuses/retweet.json", options.merge({ "id" => tweet_id.to_s }))
+        Twitter::Tweet.from_json(response)
+      end
+
+      def retweet(tweet : Twitter::Tweet, options = {} of String => String) : Twitter::Tweet
+        retweet(tweet.id)
+      end
+
+      def unretweet(tweet_id : Int32 | Int64, options = {} of String => String) : Twitter::Tweet
+        response = post("/1.1/statuses/unretweet.json", options.merge({ "id" => tweet_id.to_s }))
+        Twitter::Tweet.from_json(response)
+      end
+
+      def unretweet(tweet : Twitter::Tweet, options = {} of String => String) : Twitter::Tweet
+        unretweet(tweet.id)
       end
     end
   end
